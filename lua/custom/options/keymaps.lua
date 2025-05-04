8e45116e5d34b5a17b58f31e006ab68b163c9d56
ayushmaan-mindrops <ayushmaan.rajput@mindrops.com>
2025-05-04 10:26:34 +0530

    New cahnges

--- lua/custom/options/keymaps.lua
+++ lua/custom/options/keymaps.lua
@@ -24,6 +24,33 @@ vim.keymap.set('n', '<C-s>', function()
   harpoon:list():select(4)
 end)
 
+vim.keymap.set('n', '<leader>r1', function()
+  local item = harpoon:list():get(1)
+  if item then
+    harpoon:list():remove(item)
+  end
+end)
+
+vim.keymap.set('n', '<leader>r2', function()
+  local item = harpoon:list():get(2)
+  if item then
+    harpoon:list():remove(item)
+  end
+end)
+
+vim.keymap.set('n', '<leader>r3', function()
+  local item = harpoon:list():get(3)
+  if item then
+    harpoon:list():remove(item)
+  end
+end)
+
+vim.keymap.set('n', '<leader>r4', function()
+  local item = harpoon:list():get(4)
+  if item then
+    harpoon:list():remove(item)
+  end
+end)
 -- Toggle previous & next buffers stored within Harpoon list
 vim.keymap.set('n', '<C-S-P>', function()
   harpoon:list():prev()
